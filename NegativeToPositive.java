f6a3847b426ef7d960bcac8e152f7acec46ba150
swathi1119 <SWATHIVEERAMREDDY9@GMAIL.COM>
2021-09-04 23:24:54 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,18 @@
+package Week1.day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int negativeNumber ;
+		negativeNumber = -40;
+		int positiveNumber;
+		if(negativeNumber < 0) {
+			positiveNumber = negativeNumber * (-1);
+			System.out.println("Negative number is converted as  positive number " +positiveNumber);
+		}
+		
+
+	}
+
+}
