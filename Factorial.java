6e9d1c2c30c0271fe8e8d8774874a96831498661
swathi1119 <SWATHIVEERAMREDDY9@GMAIL.COM>
2021-09-06 15:38:51 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package Week1.assignments;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=6,fact=1;
+		
+		for(int i = 1; i <= number; i++) {
+			
+			fact = fact*i;
+		
+		}
+		System.out.println(fact);
+
+	}
+
+}
